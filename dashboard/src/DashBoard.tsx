884d6ac8896d6dced8ccbfa32c9191efec91f7e0
cynthia-isutsa <isutsacynthia@gmail.com>
2024-07-31 11:06:05 +0300

    Collapsed Sider/ Card/ Typography

--- dashboard/src/DashBoard.tsx
+++ dashboard/src/DashBoard.tsx
@@ -1,53 +1,76 @@
-import { ContactsFilled, DatabaseFilled, HomeFilled, UserOutlined } from "@ant-design/icons";
+import {
+  ContactsFilled,
+  DatabaseFilled,
+  HomeFilled,
+  MenuFoldOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import "./styles/dashboard.css";
-import { Layout, Menu } from "antd";
+import { Card, Layout, Menu, Typography } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
+import { useState } from "react";
 
 const DashBoard = () => {
+  const [collasped, setCollasped] = useState<any>();
+
   return (
     <Layout className="container">
-      <Header style={{backgroundColor: "white"}}>
-        <div className="brand">
-          Cool-Dashboard
+      <Header style={{ backgroundColor: "white" }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <MenuFoldOutlined
+            onClick={() => setCollasped(!collasped)}
+            size={27}
+            style={{ marginRight: 20, fontSize: 27 }}
+          />
+          <div className="brand">Cool-Dashboard</div>
         </div>
       </Header>
       <Layout>
-        <Sider theme="light">
-          <Menu 
-          mode="inline"
-          items={[
-            {
-              label: "Home",
-              key: "home",
-              icon: <HomeFilled />,
-              children: [
-                {
-                label: "Profile",
-                key: "profile",
-                icon: <ContactsFilled />
-                },
-                {
-                  label: "Users",
-                  key: "users",
-                  icon: <UserOutlined />
-                }
-              ]
-            },
-            {
-              label: "About us",
-              key: "aboutus",
-              icon: <DatabaseFilled />
-            },
-          ]}/>
+        <Sider theme="light" collapsed={collasped}>
+          <Menu
+            mode="inline"
+            items={[
+              {
+                label: "Home",
+                key: "home",
+                icon: <HomeFilled />,
+                children: [
+                  {
+                    label: "Profile",
+                    key: "profile",
+                    icon: <ContactsFilled />,
+                  },
+                  {
+                    label: "Users",
+                    key: "users",
+                    icon: <UserOutlined />,
+                  },
+                ],
+              },
+              {
+                label: "About us",
+                key: "aboutus",
+                icon: <DatabaseFilled />,
+              },
+            ]}
+          />
         </Sider>
-        <Content>
-          <h1>Content</h1>
+        <Content className="content">
+          <Card>
+            <Typography.Title>Sales Department</Typography.Title>
+            <Typography.Paragraph>
+              This example demonstrates the basic structure and functionality
+              for a CRUD application in React. You have components for creating,
+              reading (listing), updating, and deleting files, with the main
+              state management handled in the App component. This is a simple
+              example and can be extended with more complex state management,
+              validation, and styles as needed.
+            </Typography.Paragraph>
+          </Card>
         </Content>
       </Layout>
-      <Footer>
-        Footer
-    </Footer>
+      <Footer>Footer</Footer>
     </Layout>
   );
 };
