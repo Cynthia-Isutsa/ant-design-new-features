b5370ce9f7fc15f03f33235ab4a7e228874f03f7
cynthia-isutsa <isutsacynthia@gmail.com>
2024-08-01 10:06:47 +0300

    Row/ typography/Col/Space

--- dashboard/src/DashBoard.tsx
+++ dashboard/src/DashBoard.tsx
@@ -3,13 +3,15 @@ import {
   DatabaseFilled,
   HomeFilled,
   MenuFoldOutlined,
+  MoneyCollectOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 import "./styles/dashboard.css";
-import { Card, Layout, Menu, Typography } from "antd";
+import { Button, Card, Col, Divider, Layout, Menu, Row, Space, Typography } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
 import { useState } from "react";
+const { Text, Link } = Typography;
 
 const DashBoard = () => {
   const [collasped, setCollasped] = useState<any>();
@@ -57,17 +59,118 @@ const DashBoard = () => {
           />
         </Sider>
         <Content className="content">
-          <Card>
-            <Typography.Title>Sales Department</Typography.Title>
+          <Space direction="horizontal">
+            {/* <Card>
+            <Typography.Title level={1}>Sales Department</Typography.Title>
             <Typography.Paragraph>
-              This example demonstrates the basic structure and functionality
-              for a CRUD application in React. You have components for creating,
-              reading (listing), updating, and deleting files, with the main
-              state management handled in the App component. This is a simple
-              example and can be extended with more complex state management,
-              validation, and styles as needed.
+              Typography is used when you need to display a title or paragraph
+              contents in Articles/Blogs/Notes. can also be used When you need
+              copyable/editable/ellipsis texts.
             </Typography.Paragraph>
-          </Card>
+            <Typography.Paragraph>
+              In the process of internal desktop applications development, many
+              different design specs and implementations would be involved,
+              which might cause designers and developers difficulties and
+              duplication and reduce the efficiency of development. After
+              massive project practice and summaries, Ant Design, a design
+              language for background applications, is refined by Ant UED Team,
+              which aims to uniform the user interface specs for internal
+              background projects, lower the unnecessary cost of design
+              differences and implementation and liberate the resources of
+              design and front-end development.
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+              <Space direction="vertical">
+                <Text>Ant Design (default Text)</Text>
+                <Text type="secondary">Ant Design (secondary)</Text>
+                <Text type="success">Ant Design (success)</Text>
+                <Text type="warning">Ant Design (warning)</Text>
+                <Text type="danger">Ant Design (danger)</Text>
+                <Text disabled>Ant Design (disabled)</Text>
+                <Text mark>Ant Design (mark)</Text>
+                <Text code>Ant Design (code)</Text>
+                <Text keyboard>Ant Design (keyboard)</Text>
+                <Text underline>Ant Design (underline)</Text>
+                <Text delete>Ant Design (delete)</Text>
+                <Text strong>Ant Design (strong)</Text>
+                <Text italic>Ant Design (italic)</Text>
+                <Link href="https://ant.design" target="_blank">
+                  Ant Design (Link)
+                </Link>
+              </Space>
+            </Typography.Paragraph>
+            <Typography.Paragraph
+              copyable={{
+                text: async () =>
+                  new Promise((resolve) => {
+                    setTimeout(() => {
+                      resolve("Request text");
+                    }, 500);
+                  }),
+              }}
+            >
+              Request copy text.
+            </Typography.Paragraph>
+            
+          </Card> */}
+            <Card>
+              <Typography.Title level={4}>Sales</Typography.Title>
+              <Space direction="horizontal">
+                <MoneyCollectOutlined />
+                <Text strong>Total Sales</Text>
+              </Space>
+              <Typography.Title level={3}> $ 4,0000</Typography.Title>
+            </Card>
+            <Card>
+              <Typography.Title level={4}>Sales</Typography.Title>
+              <Space direction="horizontal">
+                <MoneyCollectOutlined />
+                <Text strong>Total Sales</Text>
+              </Space>
+              <Typography.Title level={3}> $ 4,0000</Typography.Title>
+            </Card>
+            <Card>
+              <Typography.Title level={4}>Sales</Typography.Title>
+              <Space direction="horizontal">
+                <MoneyCollectOutlined />
+                <Text strong>Total Sales</Text>
+              </Space>
+              <Typography.Title level={3}> $ 4,0000</Typography.Title>
+            </Card>
+            <Card>
+              <Typography.Title level={4}>Sales</Typography.Title>
+              <Space direction="horizontal">
+                <MoneyCollectOutlined />
+                <Text strong>Total Sales</Text>
+              </Space>
+              <Typography.Title level={3}> $ 4,0000</Typography.Title>
+            </Card>
+            <Card>
+              <Typography.Title level={4}>Sales</Typography.Title>
+              <Space direction="horizontal">
+                <MoneyCollectOutlined />
+                <Text strong>Total Sales</Text>
+              </Space>
+              <Typography.Title level={3}> $ 4,0000</Typography.Title>
+            </Card>
+          </Space>
+          <Divider />
+          <Row gutter= {10}>
+            <Col span={8}>
+            <Card>
+            <Typography.Title level={1}>Sales Department</Typography.Title>
+            <Typography.Paragraph>
+              Typography is used when you need to display a title or paragraph
+              contents in Articles/Blogs/Notes. can also be used When you need
+              copyable/editable/ellipsis texts.
+            </Typography.Paragraph>
+            </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={18}>
+            </Col>
+          </Row>
         </Content>
       </Layout>
       <Footer>Footer</Footer>
